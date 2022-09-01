45bf71cd2fabd78c5f4beed04a262af9bf9638f5
Abdullah Rasheed <54598344+AbdullahRash33d@users.noreply.github.com>
2022-09-01 14:18:24 +0500

    Update lab.py

--- lab.py
+++ lab.py
@@ -1,61 +1,15 @@
 import os, sys, subprocess, base64
 
-command = str(base64.b64decode(sys.argv[1]).decode())
-pin = 123456
-username = "user"
-password = "root"
-
-os.system(f"useradd -m {username}")
-os.system(f"adduser {username} sudo")
-os.system(f"echo '{username}:{password}' | sudo chpasswd")
-os.system("sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd")
-
-
-class RDP:
-    def __init__(self):
-        os.system("apt update")
-        self.installRDP()
-        self.installDesktopEnvironment()
-        self.installGoogleChorme()
-        self.finish()
-
-    @staticmethod
-    def installRDP():
-        subprocess.run(['wget', 'https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb'], stdout=subprocess.PIPE)
-        subprocess.run(['dpkg', '--install', 'chrome-remote-desktop_current_amd64.deb'], stdout=subprocess.PIPE)
-        subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'], stdout=subprocess.PIPE)
-
-    @staticmethod
-    def installDesktopEnvironment():
-        os.system("export DEBIAN_FRONTEND=noninteractive")
-        os.system("apt install --assume-yes xfce4 desktop-base xfce4-terminal")
-        os.system("bash -c 'echo \"exec /etc/X11/Xsession /usr/bin/xfce4-session\" > /etc/chrome-remote-desktop-session'")
-        os.system("apt remove --assume-yes gnome-terminal")
-        os.system("apt install --assume-yes xscreensaver")
-        os.system("systemctl disable lightdm.service")
-
-    @staticmethod
-    def installGoogleChorme():
-        subprocess.run(["wget", "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"], stdout=subprocess.PIPE)
-        subprocess.run(["dpkg", "--install", "google-chrome-stable_current_amd64.deb"], stdout=subprocess.PIPE)
-        subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'], stdout=subprocess.PIPE)
-
-    @staticmethod
-    def finish():
-        os.system(f"adduser {username} chrome-remote-desktop")
-        command = f"{command} --pin={pin}"
-        os.system(f"su - {username} -c '{command}'")
-        os.system("service chrome-remote-desktop start")
-
-
-try:
-    if username:
-        if command == "":
-            print("Please enter authcode from the given link")
-        elif len(str(pin)) < 6:
-            print("Enter a pin more or equal to 6 digits")
-        else:
-            RDP()
-except NameError as e:
-    print("user variable not found")
-    print("Create a User First")
+os.system("apt update")
+subprocess.run(['wget', 'https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb'], stdout=subprocess.PIPE)
+subprocess.run(['dpkg', '--install', 'chrome-remote-desktop_current_amd64.deb'], stdout=subprocess.PIPE)
+subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'], stdout=subprocess.PIPE)
+os.system("export DEBIAN_FRONTEND=noninteractive")
+os.system("apt install --assume-yes xfce4 desktop-base xfce4-terminal")
+os.system("bash -c 'echo \"exec /etc/X11/Xsession /usr/bin/xfce4-session\" > /etc/chrome-remote-desktop-session'")
+os.system("apt remove --assume-yes gnome-terminal")
+os.system("apt install --assume-yes xscreensaver")
+os.system("systemctl disable lightdm.service")
+subprocess.run(["wget", "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"], stdout=subprocess.PIPE)
+subprocess.run(["dpkg", "--install", "google-chrome-stable_current_amd64.deb"], stdout=subprocess.PIPE)
+subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'], stdout=subprocess.PIPE)
