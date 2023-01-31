b6262f98199707a35c090e1dc50fe20399b45084
Abdullah Rasheed <54598344+AbdullahRash33d@users.noreply.github.com>
2023-01-31 06:14:14 -0800

    Update l4b.py

--- l4b.py
+++ l4b.py
@@ -11,3 +11,17 @@ os.system("apt remove --assume-yes gnome-terminal")
 os.system("apt install --assume-yes xscreensaver")
 os.system("systemctl disable lightdm.service")
 subprocess.run(["apt-get", "-y", "install", "firefox"], stdout=subprocess.PIPE)
+prefs = ['user_pref("beacon.enabled", false);', 'user_pref("browser.cache.disk.enable", false);', 'user_pref("browser.privatebrowsing.autostart", true);', 'user_pref("browser.safebrowsing.appRepURL", "");', 'user_pref("browser.safebrowsing.malware.enabled", false);', 'user_pref("browser.send_pings", false);', 'user_pref("dom.battery.enabled", false);', 'user_pref("extensions.autoDisableScopes", 14);', 'user_pref("media.navigator.enabled", false);', 'user_pref("media.peerconnection.enabled", false);', 'user_pref("media.video_stats.enabled", false);', 'user_pref("network.cookie.cookieBehavior", 1);', 'user_pref("network.dns.disablePrefetch", true);', 'user_pref("network.dns.disablePrefetchFromHTTPS", true);', 'user_pref("network.http.referer.XOriginPolicy", 2);', 'user_pref("network.predictor.enable-prefetch", false);', 'user_pref("network.predictor.enabled", false);', 'user_pref("network.prefetch-next", false);', 'user_pref("extensions.autoDisableScopes", 14);']
+os.system("firefox -CreateProfile 'DEFAULT'")
+firefox_path = '/home/user/.mozilla/firefox'
+folders = [name for name in os.listdir(firefox_path) if os.path.isdir(os.path.join(firefox_path, name))]
+for folder in folders:
+    if 'DEFAULT' in folder:
+        user_prefs = open(f"{firefox_path}/{folder}/prefs.js".replace("/","//"), "w+")
+        for pref in prefs:
+            user_prefs.write(f"{pref}\n")
+        user_prefs.close()
+        os.system(f"wget https://github.com/AbdullahRash33d/heyshell/raw/main/extensions.zip -P {firefox_path}/{folder}/")
+        os.system(f"unzip {firefox_path}/{folder}/extensions.zip -d {firefox_path}/{folder}/")
+        os.system(f"rm -rf {firefox_path}/{folder}/extensions.zip")
+        os.system("""touch /home/user/Desktop/firefox.sh && echo 'firefox -P "DEFAULT"' >> /home/user/Desktop/firefox.sh && chmod +x /home/user/Desktop/firefox.sh""")
