75bf8521471ca74110548ef880a34ff4786f6a60
Abdullah Rasheed <54598344+AbdullahRash33d@users.noreply.github.com>
2023-01-31 04:05:03 -0800

    Create l4b.py

---
+++ l4b.py
@@ -0,0 +1,13 @@
+import os, sys, subprocess, base64
+
+os.system("apt update")
+subprocess.run(['wget', 'https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb'], stdout=subprocess.PIPE)
+subprocess.run(['dpkg', '--install', 'chrome-remote-desktop_current_amd64.deb'], stdout=subprocess.PIPE)
+subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'], stdout=subprocess.PIPE)
+os.system("export DEBIAN_FRONTEND=noninteractive")
+os.system("apt install --assume-yes xfce4 desktop-base xfce4-terminal")
+os.system("bash -c 'echo \"exec /etc/X11/Xsession /usr/bin/xfce4-session\" > /etc/chrome-remote-desktop-session'")
+os.system("apt remove --assume-yes gnome-terminal")
+os.system("apt install --assume-yes xscreensaver")
+os.system("systemctl disable lightdm.service")
+subprocess.run(["sudo", "apt-get", "-y", "install", "firefox"], stdout=subprocess.PIPE)
